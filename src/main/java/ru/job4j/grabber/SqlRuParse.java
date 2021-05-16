2a9e637180f61e681489cc300a76d15fa9d2302a
evgeny <ivanov.evgen.v@gmail.com>
2021-05-16 22:11:18 +0500

    Parsing Sql.ru

--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -12,25 +12,6 @@ import java.util.List;
 public class SqlRuParse implements Parse {
     private final List<Post> posts= new ArrayList<>();
 
-  /*  public static void main(String[] args) throws IOException {
-        String url = "https://www.sql.ru/forum/job-offers";
-        String ln = System.lineSeparator();
-        for (int i = 0; i < 5; i++) {
-            Document doc = Jsoup.connect(url).get();
-            Elements row = doc.select(".postslisttopic");
-            Elements links = doc.select(".sort_options");
-            Elements pages = links.select("a[href]");
-            for (Element td : row) {
-                Element href = td.child(0);
-                Element par = td.parent();
-                Element date = (Element) par.childNode(11);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text() + ln + date.text());
-            }
-            url = pages.get(i).attr("href");
-        }
-    }
-   */
 
     @Override
     public List<Post> list(String link) {
