064d50eb033cce57e40f25daf3a4ed9e734a8dcb
evgeny <ivanov.evgen.v@gmail.com>
2021-05-31 21:39:46 +0500

    Full Grabber

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -7,17 +7,20 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class Grabber implements Grab {
     private final Properties cfg = new Properties();
 
     public Store store() {
-        Store store = new PsqlStore(cfg);
-
-        return store;
+        return new PsqlStore(cfg);
     }
 
     public Scheduler scheduler() throws SchedulerException {
@@ -27,29 +30,68 @@ public class Grabber implements Grab {
     }
 
     public void cfg() throws IOException {
-        try (InputStream in = new FileInputStream("post.properties")) {
+        try (InputStream in = new FileInputStream("src/main/resources/post.properties")) {
             cfg.load(in);
         }
     }
 
-
     @Override
     public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
-       /* JobDataMap data = new JobDataMap();
+        JobDataMap data = new JobDataMap();
         data.put("store", store);
         data.put("parse", parse);
         JobDetail job = newJob(GrabJob.class)
                 .usingJobData(data)
                 .build();
-
-
+        SimpleScheduleBuilder times = SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("time")))
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
     }
 
-        */
+    public static class GrabJob implements Job {
 
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            List<Post> posts = new ArrayList<>(parse.list("https://www.sql.ru/forum/job-offers"));
+            Post post;
+            for(Post p: posts) {
+                post = parse.detail(p.getLink());
+                post.setLink(p.getLink());
+                store.save(post);
+            }
+            List<Post> blogs = new ArrayList<>();
+            try {
+                blogs.addAll(store.getAll());
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            Post postById = new Post();
+            try {
+                postById = store.findById(blogs.get(0).getId());
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            System.out.println(postById);
+        }
 
+        public static void main(String[] args) throws IOException, SchedulerException {
+            Grabber grab = new Grabber();
+            grab.cfg();
+            Scheduler scheduler = grab.scheduler();
+            Store store = grab.store();
+            grab.init(new SqlRuParse(), store, scheduler);
+        }
     }
-
-
-
 }
+
+
+
+
